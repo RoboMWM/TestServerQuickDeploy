c969cb57f391911276149cfd5dcf92542a3c2caf
RoboMWM <robomwm@users.noreply.github.com>
2018-05-08 10:12:27 -0700

    when you copy-paste from terminal

--- linuxstart.sh
+++ linuxstart.sh
@@ -1,4 +1,2 @@
 wget -N -t 1 -T 10 https://ci.destroystokyo.com/job/PaperSpigot/lastSuccessfulBuild/artifact/paperclip.jar                                                                     
-java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNew
-SizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfil
-e.encoding=UTF-8 -jar paperclip.jar                                                                                                                                            
+java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfile.encoding=UTF-8 -jar paperclip.jar                                                                                                                                            
