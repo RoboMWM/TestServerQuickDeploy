759ccbb7c1d8827635ee14a947d68620189a07bb
RoboMWM <robomwm@users.noreply.github.com>
2018-05-08 10:13:07 -0700

    and ofc I did it here too

--- winstart.ps1
+++ winstart.ps1
@@ -1,4 +1,2 @@
 wget https://ci.destroystokyo.com/job/Paper/lastSuccessfulBuild/artifact/paperclip.jar -OutFile paperclip.jar
-java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNew
-SizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfil
-e.encoding=UTF-8 -jar paperclip.jar                                                                                                                                            
+java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfile.encoding=UTF-8 -jar paperclip.jar                                                                                                                                            
